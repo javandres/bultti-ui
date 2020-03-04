68cfa59a1a731d4ecac9342e1a68493f7259e382
Daniel Dunderfelt <daniel@developsuperpowers.com>
2020-03-04 11:04:12 +0200

    Equipment catalogue parser WIP.

--- src/utils/useEquipmentCatalogueParser.ts
+++ src/utils/useEquipmentCatalogueParser.ts
@@ -1,14 +1,32 @@
 import { useEffect, useState } from 'react'
 import readExcelFile from 'read-excel-file'
+import { equipmentRowsNet } from '../common/neuralnets/equipmentRows'
+import { toString } from 'lodash'
+
+const getEquipmentRows = () => {
+  const net = equipmentRowsNet()
+
+  return (rows) => {
+    return rows.map((row) => net.run(row.map((val) => toString(val))))
+  }
+}
 
 export const useEquipmentCatalogueParser = (file: File | null) => {
   const [parsedData, setParsedData] = useState<any[]>([])
 
   useEffect(() => {
     if (file) {
-      readExcelFile(file).then((rows) => {
-        let headers = []
-      })
+      const equipmentRowsParser = getEquipmentRows()
+
+      readExcelFile(file, { getSheets: true })
+        .then((sheets) => {
+          console.log(sheets)
+          return Promise.all(sheets.map(({ name }) => readExcelFile(file, { sheet: name })))
+        })
+        .then((sheets) => {
+          const equipmentRows = sheets.map((rows) => equipmentRowsParser(rows))
+          console.log(equipmentRows)
+        })
     }
   }, [file])
 
