b1d0d2514ca6f814b06204498bdeb8179caaa1a0
Daniel Dunderfelt <daniel@developsuperpowers.com>
2020-05-15 07:00:42 +0300

    Styled boolean vtable values.

--- src/reports/ListReport.tsx
+++ src/reports/ListReport.tsx
@@ -1,10 +1,11 @@
-import React from 'react'
+import React, { useCallback } from 'react'
 import styled from 'styled-components'
 import { observer } from 'mobx-react-lite'
 import Table from '../common/components/Table'
 import { ReportComponentProps } from './reportUtil'
 import ReportTableFilters from './ReportTableFilters'
 import { EmptyView } from '../common/components/Messages'
+import { toString } from 'lodash'
 
 const ListReportView = styled.div``
 
@@ -16,6 +17,18 @@ export type PropTypes<T> = ReportComponentProps<T>
 
 const ListReport = observer(
   <ItemType extends {}>({ items, columnLabels }: PropTypes<ItemType>) => {
+    const renderCellValue = useCallback((key, val) => {
+      if (typeof val === 'boolean') {
+        return (
+          <span style={{ fontSize: '1.2rem', marginTop: '-3px', display: 'block' }}>
+            {val ? '✓' : '⨯'}
+          </span>
+        )
+      }
+
+      return toString(val)
+    }, [])
+
     return (
       <ListReportView>
         <ReportTableFilters<ItemType> items={items} excludeFields={['id', '__typename']}>
@@ -25,6 +38,7 @@ const ListReport = observer(
               maxHeight={window.innerHeight * 0.6}
               items={filteredItems}
               hideKeys={['id']}
+              renderValue={renderCellValue}
               columnLabels={columnLabels}>
               <TableEmptyView>Taulukko on tyhjä.</TableEmptyView>
             </Table>
