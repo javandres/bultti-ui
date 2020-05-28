9ddbb9607c275a7c6ccd7a378df2a54c0f70e833
Daniel Dunderfelt <daniel@developsuperpowers.com>
2020-05-28 16:02:36 +0300

    Fix serve script.

--- Dockerfile
+++ Dockerfile
@@ -5,6 +5,8 @@ ENV WORK /opt/bultti-ui
 RUN mkdir -p ${WORK}
 WORKDIR ${WORK}
 
+RUN yarn global add pm2
+
 # Install app dependencies
 COPY yarn.lock ${WORK}
 COPY package.json ${WORK}
@@ -17,4 +19,4 @@ COPY .env.production ${WORK}
 # RUN yarn run test:ci
 RUN yarn run build
 
-CMD yarn run production
+CMD ["pm2-runtime", "serve.js"]
--- serve.js
+++ serve.js
@@ -1,36 +1,35 @@
-require('dotenv').config()
-
 const express = require('express')
 const path = require('path')
 const pick = require('lodash/pick')
-const mapKeys = require('lodash/mapKeys')
-const mustacheExpress = require('mustache-express')
-const serialize = require('serialize-javascript')
+const mustache = require('mustache')
+const fs = require('fs')
 
 const app = express()
-app.use(express.static(path.join(__dirname, 'build')))
-
-app.engine('html', mustacheExpress(path.join(__dirname, 'build'), 'html'))
-app.set('view engine', 'html')
-app.set('views', path.join(__dirname, 'build'))
+app.use(express.static(path.join(__dirname, 'build'), { index: false }))
 
 app.get('/check', function (req, res) {
-  return res.send('ok')
+  res.send('ok')
 })
 
 app.get('/*', function (req, res) {
+  let htmlPath = path.join(__dirname, 'build', 'index.html')
+  let html = fs.readFileSync(htmlPath, 'utf8')
+
   let envKeys = Object.keys(process.env).filter((key) => key.startsWith('REACT_APP'))
   let env = pick(process.env, envKeys)
 
   let scriptEnv = `
 <script>
-    window._ENV = ${JSON.stringify(env)}
+    window._ENV = ${JSON.stringify(env)};
 </script>
   `
 
-  res.render(path.join(__dirname, 'build', 'index.html'), {
+  let responsePage = mustache.render(html, {
     SCRIPT_ENV: scriptEnv,
   })
+
+  res.writeHead(200, '{Content-Type:text/html}')
+  res.end(responsePage)
 })
 
 app.listen(process.env.PORT || 3001, () =>
