083a44c3d35269667d367a0593c86f5a95de0730
Daniel Dunderfelt <daniel@developsuperpowers.com>
2020-10-09 14:29:31 +0300

    HSLdevcom/bultti#53 Week dropdown for execution requirements.

--- src/executionRequirement/PostInspectionExecutionRequirements.tsx
+++ src/executionRequirement/PostInspectionExecutionRequirements.tsx
@@ -1,27 +1,56 @@
-import React, { useContext } from 'react'
+import React, { useContext, useMemo, useState } from 'react'
 import styled from 'styled-components'
 import { observer } from 'mobx-react-lite'
 import { usePostInspectionBaseRequirements } from './executionRequirementUtils'
-import { Inspection } from '../schema-types'
 import ExpandableSection, {
   HeaderMainHeading,
   HeaderSection,
 } from '../common/components/ExpandableSection'
 import { Button, ButtonSize, ButtonStyle } from '../common/components/Button'
 import { InspectionContext } from '../inspection/InspectionContext'
+import Dropdown from '../common/input/Dropdown'
+import { addWeeks, eachWeekOfInterval, format, parseISO } from 'date-fns'
+import { FlexRow } from '../common/components/common'
+import { READABLE_DATE_FORMAT } from '../constants'
 
-const PostInspectionExecutionRequirementsView = styled.div``
+const PostInspectionExecutionRequirementsView = styled.div`
+  min-height: 300px;
+`
 
 export type PropTypes = {}
 
+type WeekOption = { id: number; label: string }
+
 const PostInspectionExecutionRequirements = observer(({}: PropTypes) => {
   const inspection = useContext(InspectionContext)
+  let [currentWeek, setCurrentWeek] = useState(0)
 
   let { data: baseRequirements, loading, refetch } = usePostInspectionBaseRequirements(
     inspection?.id
   )
 
-  console.log(baseRequirements)
+  let weekOptions = useMemo((): WeekOption[] => {
+    if (!inspection) {
+      return []
+    }
+
+    let inspectionPeriodInterval: Interval = {
+      start: parseISO(inspection.inspectionStartDate),
+      end: parseISO(inspection.inspectionEndDate),
+    }
+
+    let weeksInInspectionPeriod = eachWeekOfInterval(inspectionPeriodInterval, {
+      weekStartsOn: 1,
+    })
+
+    return weeksInInspectionPeriod.map((weekStart, index) => ({
+      id: index,
+      label: `${index + 1}. ${format(weekStart, READABLE_DATE_FORMAT)} - ${format(
+        addWeeks(weekStart, 1),
+        READABLE_DATE_FORMAT
+      )}`,
+    }))
+  }, [inspection])
 
   return (
     <ExpandableSection
@@ -43,7 +72,13 @@ const PostInspectionExecutionRequirements = observer(({}: PropTypes) => {
         </>
       }>
       <PostInspectionExecutionRequirementsView>
-        Requirements here
+        <FlexRow>
+          <Dropdown
+            items={weekOptions}
+            selectedItem={weekOptions.find((week) => week.id === currentWeek)}
+            onSelect={(selected) => setCurrentWeek(selected.id)}
+          />
+        </FlexRow>
       </PostInspectionExecutionRequirementsView>
     </ExpandableSection>
   )
