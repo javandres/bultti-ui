2f1eff54db4acb87e6122132f3fdd3e04f8bcd14
Daniel Dunderfelt <daniel@developsuperpowers.com>
2020-12-15 15:03:20 +0200

    HSLdevcom/bultti#68 Add some comments to useQueryData.

--- src/util/useQueryData.ts
+++ src/util/useQueryData.ts
@@ -18,19 +18,23 @@ export const useQueryData = <TData extends {} = {}, TVariables = OperationVariab
   options: QueryHookOptions<TData, TVariables> & { pickData?: string } = {},
   subscriber?: { document: DocumentNode; variables?: TVariables }
 ) => {
+  // Merge default options with provided options
   let allOptions: QueryHookOptions<TData, TVariables> = {
     errorPolicy: 'all',
     ...defaultOptions,
     ...options,
   }
 
+  // Get the pick argument if provided
   let pickData = options?.pickData || ''
 
+  // Execute the Apollo hook
   let queryData = useQuery<TData, TVariables>(query, allOptions)
 
   let { loading, error, data = {} as TData, refetch, networkStatus, subscribeToMore } =
     queryData || {}
 
+  // Sometimes refetch is unset after fast refresh (in development), so this ensures it is always callable.
   let availableRefetch = useCallback(
     async (variables?: TVariables): Promise<ApolloQueryResult<TData>> => {
       if (refetch) {
@@ -42,10 +46,12 @@ export const useQueryData = <TData extends {} = {}, TVariables = OperationVariab
     [refetch, data, loading, networkStatus]
   )
 
+  // Special treatment when a subscriber is provided
   useEffect(() => {
     if (subscriber) {
       let { document, variables = allOptions.variables } = subscriber
 
+      // Automatically subscribe to more
       subscribeToMore({
         document,
         variables,
@@ -60,8 +66,11 @@ export const useQueryData = <TData extends {} = {}, TVariables = OperationVariab
     }
   }, [subscriber, subscribeToMore])
 
+  // Save the previous result here so we can show it while the query is refectched.
   let prevData = useRef(undefined)
 
+  // Pick the result data from the returned data structure.
+  // Return prevData if the result is not yet loaded.
   let pickedData = useMemo(() => {
     let resultData = pickGraphqlData(data, pickData)
 
