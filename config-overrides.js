ae711c57a940bb01fca017fa1001cefa80e16902
Daniel Dunderfelt <daniel@developsuperpowers.com>
2020-12-30 10:57:45 +0200

    HSLdevcom/bultti#40 Update dependencies, remove reduntant dependencies.

--- config-overrides.js
+++
@@ -1,8 +0,0 @@
-const { override, addBabelPlugins } = require('customize-cra')
-const { addReactRefresh } = require('customize-cra-react-refresh')
-
-module.exports = override(
-  // Add some babel plugins
-  ...addBabelPlugins("polished", "styled-components"),
-  addReactRefresh()
-)
