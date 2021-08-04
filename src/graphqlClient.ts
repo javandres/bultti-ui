7722bdeb77d6d40a033d0dd973df5a5e41fe35e9
Daniel Dunderfelt <daniel@developsuperpowers.com>
2021-08-04 10:11:27 +0300

    HSLdevcom/bultti#148 Add merge function for departure blocks field (issue came up during testing of 424)

--- src/graphqlClient.ts
+++ src/graphqlClient.ts
@@ -84,6 +84,9 @@ export const createGraphqlClient = (onAuthError: () => unknown = () => {}) => {
           observedExecutionRequirements: {
             merge: cacheMerge,
           },
+          inspectionDepartureBlocks: {
+            merge: cacheMerge,
+          },
         },
       },
       PreInspection: {
