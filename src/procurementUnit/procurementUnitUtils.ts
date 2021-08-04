d99d3f00247e0376bc428e9ec18786dbd1f1ff50
Daniel Dunderfelt <daniel@developsuperpowers.com>
2021-08-04 12:54:51 +0300

    HSLdevcom/bultti#424 Add comment to max age calculation function.

--- src/procurementUnit/procurementUnitUtils.ts
+++ src/procurementUnit/procurementUnitUtils.ts
@@ -3,6 +3,12 @@ import { getDateObject } from '../util/formatDate'
 import { differenceInMonths, differenceInYears, isBefore } from 'date-fns'
 import { round } from '../util/round'
 
+// Calculate the maximum age value of the unit. It starts with the base "maximumAverageAge" value, and
+// if the unit uses option time, the maximum age is increased. There are tqo methods for increasing
+// the maximum age value, by 0.5 years per option year or monthly. This function calculates
+// the increase based on when the option period started and the reference date provided.
+// This same function also exists on the server, so if you touch this, also make the
+// changes in the server function of the same name.
 export function calculateMaximumAverageAge(
   procurementUnit: ProcurementUnit,
   referenceDate: string | Date
