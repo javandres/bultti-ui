930068ce6a75511a1346be301d7cbe8c62dbfa7d
niemijoe <joe.niemi@cgi.com>
2021-09-01 12:59:02 +0300

    Getting entityCountMap with cy.wrap and using alias for it

--- cypress/integration/report/equipment_color_violation_spec.js
+++ cypress/integration/report/equipment_color_violation_spec.js
@@ -1,16 +1,11 @@
-import {
-  getDeparturesPerUnitCount,
-  getEquipmentPerCatalogueCount,
-} from '../../support/expectedEntityCounts'
-
 describe('Equipment color violation', () => {
   before(() => {
     cy.loginAdmin()
     cy.generateTestData()
-    cy.getEntityCountMap()
   })
 
   beforeEach(() => {
+    cy.getEntityCountMap()
     cy.visitAndSpy('/')
   })
 
@@ -25,39 +20,44 @@ describe('Equipment color violation', () => {
     cy.loginAdmin()
     cy.openTestPreInspection()
 
-    cy.wait(1000)
-
     cy.getTestElement('inspection_tabs_tab_reports').click()
-
     cy.getTestElement('inspection_reports_report_section_equipmentColor').click()
 
-    let expectedRowCount = getDeparturesPerUnitCount() / getEquipmentPerCatalogueCount()
-    cy.getTestElement('table_totalRows').contains(expectedRowCount)
+    cy.getDeparturesPerUnitCount().then((departuresPerUnitCount) => {
+      cy.getEquipmentPerCatalogueCount().then((equipmentPerCatalogueCount) => {
+        let expectedRowCount = departuresPerUnitCount / equipmentPerCatalogueCount
+        cy.getTestElement('table_totalRows').contains(expectedRowCount)
+      })
+    })
   })
 
   it('Gets correct count of post-inspection color violations', () => {
     cy.loginAdmin()
     cy.openTestSanctionPostInspection()
 
-    let expectedRowCount = getDeparturesPerUnitCount() / getEquipmentPerCatalogueCount()
+    cy.getDeparturesPerUnitCount().then((departuresPerUnitCount) => {
+      cy.getEquipmentPerCatalogueCount().then((equipmentPerCatalogueCount) => {
+        let expectedRowCount = departuresPerUnitCount / equipmentPerCatalogueCount
 
-    // Check from sanctions tab
-    cy.getTestElement('inspection_tabs_tab_sanction').click()
-    cy.filterTableByValueAndKey(
-      'EXTERIOR_COLOR_VIOLATION',
-      'dropdown_sanctionReason',
-      'edit_sanctions',
-      expectedRowCount
-    )
+        // Check from sanctions tab
+        cy.getTestElement('inspection_tabs_tab_sanction').click()
+        cy.filterTableByValueAndKey(
+          'EXTERIOR_COLOR_VIOLATION',
+          'dropdown_sanctionReason',
+          'edit_sanctions',
+          expectedRowCount
+        )
 
-    // Check from reports tab
-    cy.getTestElement('inspection_tabs_tab_reports').click()
-    cy.getTestElement('inspection_reports_report_section_sanctionList').click()
-    cy.filterTableByValueAndKey(
-      'EXTERIOR_COLOR_VIOLATION',
-      'dropdown_sanctionReason',
-      'inspection_reports_report_sanctionList_table',
-      expectedRowCount
-    )
+        // Check from reports tab
+        cy.getTestElement('inspection_tabs_tab_reports').click()
+        cy.getTestElement('inspection_reports_report_section_sanctionList').click()
+        cy.filterTableByValueAndKey(
+          'EXTERIOR_COLOR_VIOLATION',
+          'dropdown_sanctionReason',
+          'inspection_reports_report_sanctionList_table',
+          expectedRowCount
+        )
+      })
+    })
   })
 })
--- cypress/support/expectedEntityCounts.js
+++ cypress/support/expectedEntityCounts.js
@@ -1,51 +1,65 @@
-let entityCountMap = null // TODO: start using typescript and add an interface of this variable
+// Seems like there is no solution to keep aliases in memory between the tests:
+// https://github.com/cypress-io/cypress/issues/665#issuecomment-573625696
+//
+// So the solution is to run getEntityCountMap() in beforeEach hook AND we'll store the response in a regular variable: entityCountMap
+// TODO: if you find a way to keep alias in memory where you can just run this command once, remove usage of
+// this regular variable.
+let entityCountMapCache = null // TODO: start using typescript and add an interface of this variable
 
 Cypress.Commands.add('getEntityCountMap', () => {
-  // Use cache if found
-  if (entityCountMap) {
-    return entityCountMap
+  if (entityCountMapCache) {
+    cy.wrap(entityCountMapCache).as('entityCountMap')
+  } else {
+    let requestEntityCountMap = new Promise((resolve, reject) => {
+      const REACT_APP_SERVER_URL = Cypress.env('CYPRESS_REACT_APP_SERVER_URL')
+      const testDataConfigQuery = `
+        query testDataConfig {
+          testDataConfig
+        }
+      `
+      cy.request({
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/graphql',
+        },
+        form: true,
+        url: `${REACT_APP_SERVER_URL}/graphql`,
+        body: { query: testDataConfigQuery, operationName: 'testDataConfig' },
+      }).then((res) => {
+        if (!res.body?.data?.testDataConfig) {
+          console.log(
+            'TestDataConfig query failed. Received res.body did not contain testDataConfig.'
+          )
+          reject()
+        }
+        let entityCountMap
+        try {
+          entityCountMap = JSON.parse(res.body.data?.testDataConfig)
+        } catch (e) {
+          console.log('JSON.parse on testDataConfig failed.')
+          reject()
+        }
+        // Put the response in memory so that we don't have to make a request each time a test is ran
+        entityCountMapCache = entityCountMap
+        resolve(entityCountMap)
+      })
+    })
+    cy.wrap(requestEntityCountMap).as('entityCountMap')
   }
-
-  if (entityCountMap) {
-    return entityCountMap
-  }
-
-  const testDataConfigQuery = `
-    query testDataConfig {
-      testDataConfig
-    }
-  `
-
-  const REACT_APP_SERVER_URL = Cypress.env('CYPRESS_REACT_APP_SERVER_URL')
-  cy.request({
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/graphql',
-    },
-    form: true,
-    url: `${REACT_APP_SERVER_URL}/graphql`,
-    body: { query: testDataConfigQuery, operationName: 'testDataConfig' },
-  }).then((res) => {
-    if (!res.body?.data?.testDataConfig) {
-      throw new Error(
-        `TestDataConfig query failed. Received res.body did not contain testDataConfig.`
-      )
-    }
-    entityCountMap = JSON.parse(res.body.data?.testDataConfig)
-
-    return entityCountMap
-  })
 })
 
-export function getDeparturesPerUnitCount() {
-  return (
-    entityCountMap.routes *
-    entityCountMap.directions *
-    entityCountMap.dayTypes *
-    entityCountMap.departures
-  )
-}
-
-export function getEquipmentPerCatalogueCount() {
-  return entityCountMap.equipmentPerCatalogue
-}
+Cypress.Commands.add('getDeparturesPerUnitCount', () => {
+  cy.get('@entityCountMap').then((entityCountMap) => {
+    return (
+      entityCountMap.routes *
+      entityCountMap.directions *
+      entityCountMap.dayTypes *
+      entityCountMap.departures
+    )
+  })
+})
+Cypress.Commands.add('getEquipmentPerCatalogueCount', () => {
+  cy.get('@entityCountMap').then((entityCountMap) => {
+    return entityCountMap.equipmentPerCatalogue
+  })
+})
